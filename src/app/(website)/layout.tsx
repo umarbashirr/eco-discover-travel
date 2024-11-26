e60ea38fbd3349c5ac8f4dffbf137cecdbd6e54d
umarbashirr <umarbashir93@outlook.com>
2024-11-26 17:26:19 +0530

    added google tag manager

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -2,15 +2,17 @@ import { Footer } from "@/components/footer";
 import { MainNavbar } from "@/components/main-navbar";
 import { TopNavbar } from "@/components/top-navbar";
 import React from "react";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const WebsiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <>
       <TopNavbar />
       <MainNavbar />
       <main className="w-full min-h-screen">{children}</main>
       <Footer />
-    </div>
+      <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID as string} />
+    </>
   );
 };
 
