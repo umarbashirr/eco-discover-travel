e3af440dae6c913ed9566b3848c1346a8ec8dd56
umarbashirr <umarbashir93@outlook.com>
2024-11-27 14:36:15 +0530

    unused image element removed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,5 @@
 import { otherRoutes, routes } from "@/data/routes";
 import { Facebook, Instagram, Youtube } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { Container } from "./container";
 import { Separator } from "./ui/separator";
